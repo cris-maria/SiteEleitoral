9f3d3df238e2bf5a09011618b49cf21a528b3b6d
cris-maria <53320569+cris-maria@users.noreply.github.com>
2020-07-22 17:03:44 -0300

    Add files via upload

---
+++ Intensão/js/main.js
@@ -0,0 +1,12 @@
+function redirecionarPrincipal() {
+    document.location.assign("./Pages/index.html")
+
+}
+
+function redirecionarCadastro() {
+    document.location.assign("./Pages/cadastrarUsuario.html")
+}
+
+function retornarLogin(){
+    document.location.assign("./login.html")
+}
